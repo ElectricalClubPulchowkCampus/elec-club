cc9f2b2ec9f12ee39f1ac93ba84ad92d00f4d223
bin1o1 <binit.neupane51@gmail.com>
2025-05-31 17:53:19 +0545

    Deployment issues solved

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/elec-club-react/',
+  base: '/elec-club/',
   plugins: [
     react(), 
     tailwindcss()
