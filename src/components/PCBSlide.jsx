67a07c296b30b2a8e67b18c7254bf9d53b8f3426
bin1o1 <binit.neupane51@gmail.com>
2025-06-17 20:18:41 +0545

    corrected workshop name

--- src/components/PCBSlide.jsx
+++ src/components/PCBSlide.jsx
@@ -11,8 +11,11 @@ export default function PCBSlide() {
           <div className="absolute inset-0 bg-black/60 backdrop-blur-sm z-0"></div>
 
           <div className="absolute inset-0 z-10 flex flex-row items-center justify-center">
-            <div className="text-5xl p-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-300 via-purple-400 to-pink-400 text-center flex flex-col items-center justify-center space-y-2">
+            <div className="text-5xl p-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 via-yellow-350 to-pink-500 text-center flex flex-col items-center justify-center space-y-2">
                 Proteus: PCB Design and Fabrication Workshop
+                <p className="text-2xl p-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 via-yellow-350 to-yellow-500 text-center flex flex-col items-center justify-center space-y-2">
+                    Electrified by Litmus Cables
+                </p>
                 <p className="text-white text-base pt-8 font-bold">
                     📅 June 20  –23, 2025 
                 </p>
@@ -26,7 +29,7 @@ export default function PCBSlide() {
                 href="https://docs.google.com/forms/d/e/1FAIpQLSeqXQJhmke5LU_jaCBMKY2RcIX2Y6vh45g6bFU7WPFx32NYEA/viewform?fbclid=IwY2xjawK6AzFleHRuA2FlbQIxMABicmlkETFJSU5HSGE4SERDSVcxZ0kwAR4Y2U4jXjQg46nk_vOBYYCThEsGFj6fSQCN68t2CVrVqtKxgtVQFsWHTNtRdQ_aem_Jgu72x6J7sS5gegNGBeAvQ"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="mt-6 inline-block px-8 py-3 bg-gradient-to-r from-sky-400 via-indigo-500 to-purple-600 text-white font-semibold text-lg rounded-full shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-300"
+                className="mt-6 inline-block px-8 py-3 bg-gradient-to-r from-purple-200 via-yellow-350 to-pink-500 text-black font-semibold text-lg rounded-full shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-300"
                 >
                     🚀 Register Now !
                 </a>
